ea2230aa1ea2f5b2fbd4805912aba869dc9f3f18
Andrew Shirley <andrew@als-bi.com>
2018-10-28 22:49:16 +0000

    Fix time progression and add palette rotation

--- library.h
+++ library.h
@@ -1,25 +1,37 @@
-CRGB *bl_palette;
-
 CRGB *bl_ledTarget;
 CRGB *bl_leds;
 int bl_ledCount;
 
+//*****
+// LEDs show colors from a palette which rotates over time
+
+CRGB *bl_palette;
+int bl_paletteCount;
+int bl_palettePeriod = 10; //time to cycle through the whole palette
+
+//*****
+// LEDs can "twinkle" i.e. randomly change quickly to this color then slowly back (typically CRGB::White )
+
 CRGB bl_twinkleColor = 0;
 byte bl_twinkleChance = 0;
-int bl_twinkleSustainTime = 300;
+int bl_twinkleSustainTime = 100;
 long *bl_twinkleTimes;
 
+//*****
+// Time itself can progress faster/slower based on a sine wave.
+
 uint8_t bl_timePeriod = 40; //0; //(x/4)Hz or 0 for no effect.
-uint8_t bl_timeAmplitude = 255; // 0 = no effect, 255 ~= 12 times speed difference between slowest and fastest.
+uint8_t bl_timeAmplitude = 64; // 0 = no effect, 255 ~= 12 times speed difference between slowest and fastest.
 uint8_t bl_timePhase = 0; //
 
 long _time = 0;
 long _wallTime = 0;
 
-void blinken_init(CRGB *palette, CRGB* leds, int ledCountIn) {
+void blinken_init(CRGB *palette, int paletteCount, CRGB* leds, int ledCountIn) {
   bl_palette = palette;
-  bl_ledCount = ledCountIn;
+  bl_paletteCount = paletteCount;
   bl_leds = leds;
+  bl_ledCount = ledCountIn;
   bl_ledTarget = (CRGB *) malloc(ledCountIn * sizeof(CRGB));
   bl_twinkleTimes = (long *) malloc(ledCountIn * sizeof(long));
   memset(bl_twinkleTimes, 0, ledCountIn * sizeof(long));
@@ -42,56 +54,63 @@ void blinken_setTwinkle(CRGB color, byte chance) {
 
 void blinken_show() {
   //advance time
+
+  //Work out how much actual time (wall time) has passed.
+  //Then use a sine wave to determine how much of that wallTime change we will add onto our virtual time.
+  // TODO: When we apply this effect we speed time up on average which I would prefer to avoid
+  // also, there is a discontinuity on the graph of speed of time which is upsetting!
+
   long prevWallTime = _wallTime;
   _wallTime = millis();
   int wallTimeStep = (int) (_wallTime - prevWallTime);
 
-  long timeSineAdj = 128*255; //meaning use wallTime as-is
-  uint8_t amplitudeMultiple;
+  long timeSineAdj = 512; //default to using wallTime as-is
   if (bl_timePeriod) {
-    //              0-256              -128-128   0-256
-    timeSineAdj = (bl_timeAmplitude * (128 - sin8((uint8_t) (_wallTime / bl_timePeriod) + bl_timePhase)) / 64) + 128
-    //timeSineAdj is between z -> 256 where z = 256/multiple
-//    byte z = 256L * 16 / bl_timeAmplitude;
-//    timeSineAdj = ((long) z * 255 + (256L - z) * sin8((uint8_t) (_wallTime / bl_timePeriod) + bl_timePhase)) / (128L - z);
-    //20 ->240 ish
-//    timeSineAdj = (20L + (5L * bl_timeAmplitude * (sin8((uint8_t) (_wallTime / bl_timePeriod) + bl_timePhase)) / 6) / 255);
+    //a: sin is 0->255 (128 is neutral)
+    // b: -128, multiply by (bl_timeAmplitude / 64) (0->4), + 512 = 0->1024 (512 is neutral)
+    // 0    means use 1/4 * wallTimeStep
+    // 256  means use 1/2 * wallTimeStep
+    // 512  means use  1  * wallTimeStep
+    // 768  means use  2  * wallTimeStep
+    // 1024 means use  4  * wallTimeStep
+
+    int a = sin8((uint8_t) (_wallTime / bl_timePeriod) + bl_timePhase);
+    int b = (bl_timeAmplitude * (a - 128) / 64) + 512;
+    //TODO: Can this be done mathmatically?
+    if (b == 512) {
+      timeSineAdj = 512; // / 512
+    } else if (b < 512) {
+      timeSineAdj = 128 + (3L * 128 * b / 512);// / 512  //between 512 and 128
+    } else {
+      timeSineAdj = (b - 512) * 3 + 512;// / 512
+    }
   }
+
   long prevTime = _time;
-  _time = prevTime + (long) wallTimeStep * timeSineAdj / 256;
+  _time = prevTime + (long) wallTimeStep * timeSineAdj / 512;
   int timeStep = _time - prevTime;
 
-  //
-
-  Serial.print(_wallTime);
-  Serial.print("\t");
-  Serial.print(wallTimeStep);
-  Serial.print("\t");
-  Serial.print(timeSineAdj);
-  Serial.print("\t");
-  Serial.print(timeSineAdj / 256);
-  Serial.print("\t");
-  Serial.print(_time);
-  Serial.print("\t");
-  Serial.print(timeStep);
-  Serial.println("");
-
+  int paletteIndex;
   for (byte i = 0; i < bl_ledCount; i++) {
     //twinkle?
     if (bl_twinkleChance && (random16() <= ((long) bl_twinkleChance * 256 * timeStep / 1000))) {
       bl_twinkleTimes[i] = _time;
     }
 
-    bl_ledTarget[i] = bl_palette[0];
+    paletteIndex = map(((_time / (bl_palettePeriod)) % 1024), 0, 1023, 0, bl_paletteCount);
+    //TODO: be more configurable how much i affects the paletteIndex.
+    paletteIndex = (paletteIndex + i) % bl_paletteCount;
+    bl_ledTarget[i] = bl_palette[paletteIndex];
 
     if (_time - bl_twinkleTimes[i] <= bl_twinkleSustainTime) {
       bl_leds[i] = bl_twinkleColor;
     }
     else {
       //fade towards target
-      bl_leds[i] = bl_leds[i].scale8(64) + bl_ledTarget[i].scale8(192); // 3/4 target + 1/4 curr.
+      bl_leds[i] = bl_leds[i].nscale8(128) + bl_ledTarget[i].nscale8(128); // 1/2 target + 1/2 curr.
     }
   }
+
   FastLED.show();
   delay(30);
 }
--- neopixel-cyber.ino
+++ neopixel-cyber.ino
@@ -11,6 +11,9 @@ CRGB leds[NUM_LEDS + NUM_LEDS2];
 
 CRGB palette[2] = {0x0000ff, 0xffffff};
 
+CRGB christmas[50] = {0xFF7400, 0xFF6400, 0xFF3700, 0xFF1300, 0xFF0000, 0xDA7A00, 0xDA8F00, 0xC98F00, 0xC96400, 0xAE7A00, 0x697A00, 0x287A00, 0x28B800, 0x28B800, 0x28B800, 0x28B800, 0x47D021, 0x47DF3D, 0x0CFF34, 0x47F421, 0x47F421, 0x47ED3D, 0x47EA3D, 0x27DD3D, 0x0CD15B, 0x0CD181, 0x0CD1B2, 0x0CD1CC, 0x0CFFD6, 0x0CDCEB, 0x0CC9EB, 0x0CA3EB, 0x006BFF, 0x156BFF, 0x596BFF, 0x7C6BFF, 0x884DFF, 0xAF3CC3, 0xAF3C75, 0xC33C40, 0xC3090C, 0xFF090C, 0xFF090C, 0xFF180C, 0xFF4A0C, 0xFF710C, 0xFF820C, 0xFFAA0C, 0xFFFA0C, 0xFFAA0C};
+const int christmasCount = 50;
+
 void setup() {
   FastLED.addLeds<WS2811, DATA_PIN>(leds, NUM_LEDS);
   //FastLED.addLeds<NEOPIXEL, DATA_PIN2>(leds + 50, NUM_LEDS2);
@@ -22,7 +25,9 @@ void setup() {
     leds[i] = CRGB::Black;
   }
 
-  blinken_init(palette, leds, NUM_LEDS/* + NUM_LEDS2*/);
+  FastLED.show();
+
+  blinken_init(christmas, 50, leds, NUM_LEDS/* + NUM_LEDS2*/);
   blinken_setTwinkle(CRGB::White, 20);
   Serial.begin(115200);
   Serial.println("Startup");
