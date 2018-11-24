fb1971f1990d2775f485afde4d11b09f17608819
Andrew Shirley <andrew@als-bi.com>
2018-11-24 16:43:57 +0000

    support more complicated layouts

--- util/generateLocations.py
+++ util/generateLocations.py
@@ -1,14 +1,42 @@
 from __future__ import print_function
 
-numLeds = 50
-width = 4
+# ----  |   height + 2
+# |  |  |   height + 1
+# |  |  |   2
+# |  ----   1
+#
+# numLeds = 16
+# width = 4
+# turn = 2
+# => height = 2
 
-length = (numLeds -width + 2) / 2
+numLeds = 200
+width = 2
+turns = 15
 
-locStr = "Location ledLocations[] = {" + (", ".join(
-    ["Location({}, 1)".format(x) for x in range(1, length + 1)] +
-    ["Location({}, {})".format(length, y) for y in range(2, width)] +
-    ["Location({}, {})".format(x, width) for x in range(length, 0, -1)]
-)) + "};"
+height = (numLeds -(width * turns) - 2) / (turns + 1)
+
+#initialise with first upwards height (with an extra led in the bottom as there is no width coming in)
+down = True
+currX = 1
+locs = ["Location(1, {})".format(y) for y in range(1, height + 2)]
+for i in range(turns):
+    if down:
+        locs += ["Location({}, {})".format(x, height + 2) for x in range(currX, currX + width)]
+        locs += ["Location({}, {})".format(currX + width - 1, y) for y in range(height + 1, 1, -1)]
+    else:
+        locs += ["Location({}, {})".format(x, 1) for x in range(currX, currX + width)]
+        locs += ["Location({}, {})".format(currX + width - 1, y) for y in range(2, height + 2)]
+    down = not down
+    currX += width - 1
+
+if down:
+    locs += ["Location({}, {})".format(currX, height + 2)]
+else:
+    locs += ["Location({}, {})".format(currX, 1)]
+
+leftOver = numLeds - (2 + height * (turns + 1) + width * turns)
+
+locStr = "Location ledLocations[] = {" + (", ".join(locs)) + "};\n#leftOver = " + str(leftOver)
 
 print(locStr);
