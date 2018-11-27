76645adfa9ebfcc87deaf79a5704efdf9bf88768
Andrew Shirley <andrew@als-bi.com>
2018-11-27 22:05:19 +0000

    Bulk out serial logging

--- blinkenlights.ino
+++ blinkenlights.ino
@@ -18,26 +18,42 @@ const int christmasCount = 50;
 Location ledLocations[] = {Location(1, 1), Location(2, 1), Location(3, 1), Location(4, 1), Location(5, 1), Location(6, 1), Location(7, 1), Location(8, 1), Location(9, 1), Location(10, 1), Location(11, 1), Location(12, 1), Location(13, 1), Location(14, 1), Location(15, 1), Location(16, 1), Location(17, 1), Location(18, 1), Location(19, 1), Location(20, 1), Location(21, 1), Location(22, 1), Location(23, 1), Location(24, 1), Location(24, 2), Location(24, 3), Location(24, 4), Location(23, 4), Location(22, 4), Location(21, 4), Location(20, 4), Location(19, 4), Location(18, 4), Location(17, 4), Location(16, 4), Location(15, 4), Location(14, 4), Location(13, 4), Location(12, 4), Location(11, 4), Location(10, 4), Location(9, 4), Location(8, 4), Location(7, 4), Location(6, 4), Location(5, 4), Location(4, 4), Location(3, 4), Location(2, 4), Location(1, 4)};
 
 void setup() {
+  Serial.begin(115200);
+  Serial.println("Startup");
+
+  Serial.print("Initialising FastLED...");
   FastLED.addLeds<WS2811, DATA_PIN>(leds, NUM_LEDS);
   //FastLED.addLeds<NEOPIXEL, DATA_PIN2>(leds + 50, NUM_LEDS2);
+  Serial.println(" Done");
+  Serial.print("Initialising random seeds...");
   randomSeed(analogRead(0));
   random16_set_seed(analogRead(0));
+  Serial.println(" Done");
 
+  Serial.print("Initialising LED array...");
   FastLED.setBrightness(64);
   for (byte i = 0; i < NUM_LEDS/* + NUM_LEDS2*/; i++) {
     leds[i] = CRGB::Black;
   }
+  Serial.println(" Done");
 
+  Serial.print("Showing first LED values...");
   FastLED.show();
+  Serial.println(" Done");
 
+  Serial.print("initialising blinken...");
   blinken_init(christmas, 50, leds, NUM_LEDS/* + NUM_LEDS2*/);
+  Serial.println(" Done");
+
+  Serial.print("setting blinken locations...");
   blinken_setLocations(ledLocations);
+  Serial.println(" Done");
+
+  Serial.print("setting blinken twinkle...");
   blinken_setTwinkle(CRGB::White, 20);
-  Serial.begin(115200);
-  Serial.println("Startup");
+  Serial.println(" Done");
 }
 
 void loop() {
   blinken_show();
 }
-
